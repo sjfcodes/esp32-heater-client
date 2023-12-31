a43e7d9e763ad7ae54438a92bafd3df23f399f40
sjfox <sjfcodes@gmail.com>
2023-12-30 21:29:46 -0800

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 #include <ArduinoJson.h>
 
 // Wifi Credentials
-const char *ssid = ""; // Wifi SSID
-const char *password = "";    // Wi-FI Password
+const char *ssid = "";     // Wifi SSID
+const char *password = ""; // Wifi Password
 
 // Websocket server location
-const int wsServerPort = 3001;                 // websocket server port
-const char *wsServerIp = "192.168.68.142";     // websocket server's host ip
-const char *wsServerPath = "/heaterGpioState"; // websocket server path
+const int wsServerPort = 3001;                 // Websocket server port
+const char *wsServerIp = "192.168.68.142";     // Websocket server's host ip
+const char *wsServerPath = "/heaterGpioState"; // Websocket server path
 
 // GPIO
-const int thermoPin = 33;
-const int heaterPin = 32;
-WebSocketsClient webSocket;  // websocket client class instance
+const int thermoPin = 33;    // Thermostat GPIO pin
+const int heaterPin = 32;    // Heater GPIO pin
+WebSocketsClient webSocket;  // Websocket client instance
 StaticJsonDocument<100> doc; // Allocate a static JSON document
 
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
@@ -27,7 +27,7 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
     DeserializationError error = deserializeJson(doc, payload); // deserialize incoming Json String
     if (error)
     { // Print erro msg if incoming String is not JSON formatted
-      Serial.println("deserializeJson() failed: " + String(error.c_str()));
+      Serial.println("JSON DeserializationError: " + String(error.c_str()));
       return;
     }
 
@@ -35,38 +35,35 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
     Serial.println("isOn: " + String(isOn));
     if (isOn == true)
     {
-      Serial.println("true");
-      digitalWrite(heaterPin, HIGH);
-      digitalWrite(LED_BUILTIN, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH); // Led on
+      digitalWrite(heaterPin, HIGH);   // Heat on
+      Serial.println("Heater on");     // log action
     }
     else
     {
-      Serial.println("false");
-      digitalWrite(heaterPin, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
+      digitalWrite(LED_BUILTIN, LOW); // Led off
+      digitalWrite(heaterPin, LOW);   // Heat off
+      Serial.println("Heater off");   // log action
     }
   }
 }
 
 void setup()
 {
-  // Connect to local WiFi
-  WiFi.begin(ssid, password);
-  Serial.begin(115200);
-  while (WiFi.status() != WL_CONNECTED)
+  Serial.begin(115200);                 // Connect to Terminal
+  WiFi.begin(ssid, password);           // Connect to WiFi
+  while (WiFi.status() != WL_CONNECTED) // Waiting on Wifi
   {
     Serial.print(".");
     delay(500);
   }
-  Serial.println();
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP()); // Print local IP address
-  delay(2000);                    // wait for 2s
+  Serial.println("IP : " + String(WiFi.localIP())); // Print local IP address
+  delay(2000);                                      // Pause 2s
 
-  webSocket.begin(wsServerIp, wsServerPort, wsServerPath);
-  webSocket.onEvent(webSocketEvent);
-  // if connection failed retry every 5s
-  webSocket.setReconnectInterval(5000);
+  webSocket.begin(wsServerIp, wsServerPort, wsServerPath); // Connect to ws server
+  webSocket.onEvent(webSocketEvent);                       // Handle ws events
+  webSocket.setReconnectInterval(5000);                    // Handle connection failure, retry every 5s
+
   pinMode(heaterPin, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 }
